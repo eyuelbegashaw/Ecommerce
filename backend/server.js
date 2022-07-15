08030147e7645c940890388583653b033f40cf0a
eyuel begashaw <eyuelbegashaw214@gmail.com>
2022-07-15 16:50:05 +0300

    product route

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
+//Modules
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+
+//Database
 import connectDB from "./config/db.js";
 
+//Routes
+import productRouter from "./routes/productRoutes.js";
+
 dotenv.config();
-
 connectDB();
-
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRouter);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((current) => current._id == req.params.id);
-  res.json(product);
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${5000}`));
