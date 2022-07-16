390459d2342d3f754b2ae25d76645b239f506571
eyuel begashaw <eyuelbegashaw214@gmail.com>
2022-07-16 21:27:33 +0300

    Route update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,32 @@
+import mongoose from "mongoose";
 import express from "express";
+
 const router = express.Router();
 
-import products from "../data/products.js";
+import Product from "../models/productModel.js";
 
-router.get("/", (req, res) => {
-  res.json(products);
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.find({});
+    if (products) res.json(products);
+    else res.status(404).json({ message: " No product found " });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
-  const product = products.find((current) => current._id == req.params.id);
-  res.json(product);
+router.get("/:id", async (req, res, next) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(404).json({ message: "Product Not Found" });
+    }
+
+    const product = await Product.findById(req.params.id);
+    if (product) res.json(product);
+    else res.status(404).json({ message: " Product Not Found " });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,18 @@ import connectDB from "./config/db.js";
 //Routes
 import productRouter from "./routes/productRoutes.js";
 
+//middlewares
+import { NotFound, ErrorMiddleware } from "./middlewares/errorMiddleware.js";
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use("/api/products", productRouter);
+
+app.use(NotFound);
+app.use(ErrorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
